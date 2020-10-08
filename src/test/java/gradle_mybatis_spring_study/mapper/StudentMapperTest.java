7f93390f25e4ec90cefedc3a6754f956443667ad
digi5633 <digi5633@gmail.com>
2020-10-08 12:44:18 +0900

    MyBatis select 끝!

---
+++ src/test/java/gradle_mybatis_spring_study/mapper/StudentMapperTest.java
@@ -0,0 +1,63 @@
+package gradle_mybatis_spring_study.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import gradle_mybatis_spring_study.config.ContextRoot;
+import gradle_mybatis_spring_study.dto.Student;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { ContextRoot.class })
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class StudentMapperTest {
+
+	protected static final Log log = LogFactory.getLog(StudentMapperTest.class);
+
+	@Autowired
+	private StudentMapper mapper;
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println();
+	}
+
+	@Test
+	public void test01SelectStudentByNo() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Student student = new Student();
+		student.setStudId(1);
+		Student selectStudent = mapper.selectStudentByNo(student);
+		log.debug(selectStudent.toString());
+		Assert.assertNotNull(student);
+	}
+
+	@Test
+	public void test02SelectStudentByNoWithResultMap() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Student student = new Student();
+		student.setStudId(1);
+		Student selectStudent = mapper.selectStudentByNoWithResultMap(student);
+		log.debug(selectStudent.toString());
+		Assert.assertNotNull(student);
+	}
+
+	@Test
+	public void test03SelectStudentByAll() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		List<Student> list = mapper.selectStudentByAll();
+		Assert.assertNotNull(list);
+		list.stream().forEach(System.out::println);
+	}
+
+}
